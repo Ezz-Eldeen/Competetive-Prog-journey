a3c7b5a82f35c469c7249af11b1d498cbd0a07c7
Ezz Eldeen <ezz.eldeen.mohammad.9@gmail.com>
2025-04-17 16:27:36 +0200

    Start RegularBracketSequence26B (WIP)

---
+++ ACM Sheet 1st sesh/RegularBracketSequence_26B/RegularBracketSequence_26B.cpp
@@ -0,0 +1,49 @@
+#include <iostream> 
+#include <unordered_map>
+
+int solve(){
+
+
+    std::string input;
+    std::cin >> input;
+    std::unordered_map<char, int> hash_table; 
+    for (char bracket: input){
+
+        bool second_condition = !(hash_table['('] == hash_table[')']);
+        // std::cout <<"The second condition is"<< second_condition << "\n";
+        
+        // std::cout <<"First value "<< hash_table['('] << "\n";
+        // std::cout <<"Second value "<< hash_table[')'] << "\n";
+
+        if (bracket == '('){
+            // std::cout << "invoked the if \n";
+            hash_table['('] += 1;
+        }
+
+        else if (bracket == ')' && (second_condition)){
+            // std::cout << "invoked the else \n";
+            
+            hash_table[')'] += 1;
+        }
+        
+    }
+    int count;
+
+    if ( (hash_table['('] + hash_table[')'])%2 ){
+           
+        count = (hash_table['('] + hash_table[')'])-1;
+
+    }
+    else{
+        count = (hash_table['('] + hash_table[')']);
+
+    }
+    return (count);
+}
+
+
+int main(){
+
+std::cout<< solve();
+
+}
