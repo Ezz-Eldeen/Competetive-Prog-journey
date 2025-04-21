8231c9a92635acfdeffac721c75dbe1ce563384e
Ezz Eldeen <ezz.eldeen.mohammad.9@gmail.com>
2025-04-21 14:14:25 +0200

    Fix 4C_Registration_System

--- ACM Sheet 1st sesh/Registration_System_4C/4C_Registration_System.cpp
+++ ACM Sheet 1st sesh/Registration_System_4C/4C_Registration_System.cpp
@@ -1,10 +1,11 @@
 #include <iostream> 
-#include <unordered_set> 
+// #include <unordered_set>
+#include <unordered_map> 
 #include <string> 
 
-std::unordered_set<std::string>  usernames; 
+// std::unordered_set<std::string>  usernames; 
+std::unordered_map<std::string, int> usernames;
 
-int count = 1;
 void checkName(){
 
     std::string username;
@@ -13,13 +14,13 @@ void checkName(){
     
     if(!(usernames.count(username))){
         std::cout<< "OK" << "\n";
-        usernames.insert(username);
+        usernames.insert({username, 1});
     }
     else {
-        std::string new_name = username+std::to_string(count);
-        count++;
+        std::string new_name = username+std::to_string(usernames[username]);
+        usernames[username] = usernames[username] +1 ;
         // std::cout<< "new name: " << new_name <<"\n";
-        usernames.insert(new_name);
+        // usernames.insert(new_name);
         std::cout << new_name << "\n";}
 }
 int main() {
