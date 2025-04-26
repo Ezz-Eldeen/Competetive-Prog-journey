016ad7e01e6d1ba7bbb661e86fe2ab28948d333c
Ezz Eldeen <ezz.eldeen.mohammad.9@gmail.com>
2025-04-26 15:07:44 +0300

    Submit 1133C_Balanced_Team

--- ACM Sheet 2nd sesh/1133C_Balanced_Team/1133C_Balanced_Team.cpp
+++ ACM Sheet 2nd sesh/1133C_Balanced_Team/1133C_Balanced_Team.cpp
@@ -3,55 +3,24 @@
 #include <climits>
 #include <algorithm>
 
-int checkNo(std::vector<int> &input)
-{
-
-    int min = INT_MAX;
-    int counter = 0;
-    int max = 0;
-
-    for (int i = 0; i < input.size(); i++)
-    {
-        // std::cout<< "Condition: " <<  (abs(input[i] - min) < 6) << std::endl;
-        if ((abs(input[i] - min) < 6) || (i == 0 && counter == 0))
-        {
-            // std::cout << "Invoked if \n";
-            // std::cout<< i;
-            counter++;
-            min = std::min(input[i], min);
-            // std::cout << "new min: " << min << "\n";
-        }
-        else{
-            // std::cout << "Invoked else \n";
-            counter = 1;
-            min = input[i];
-        }
-        if (max < counter)
-            max = counter;
-
-        }
-        return max;
-}
 int main()
 {
-
-    int n;
-
-    scanf("%d", &n);
-    std::vector<int> input;
-
-    for (int i = 0; i < n; i++)
-    {
-        int a;
-        scanf("%d", &a);
-        input.push_back(a);
-    }
-    
-    // std::sort(input.begin(), input.end());
-    // for (int a: input){
-    //     std::cout << a << " ";
-    // }
-    std::cout<< checkNo(input);
-
+	int n;
+	std::cin >> n;
+	std::vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i];
+	}
+	std::sort(a.begin(), a.end());
+	int ans = 0;
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		while (j < n && a[j] - a[i] <= 5) {
+			j++;
+			ans = std::max(ans, j - i);
+		}
+	}
+	
+	std::cout << ans << "\n";
     return 0;
 }
