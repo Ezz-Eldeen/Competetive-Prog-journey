eec379ef0f04aaefb390045d7250df3fcef91349
Ezz Eldeen <ezz.eldeen.mohammad.9@gmail.com>
2025-04-27 15:27:34 +0300

    Fix 1526C2_Potions(HardVerison)

--- ACM Sheet 1st sesh/Potions(HardVersion)_1526C/Potions(HardVersion)_1526C.cpp
+++ ACM Sheet 1st sesh/Potions(HardVersion)_1526C/Potions(HardVersion)_1526C.cpp
@@ -1,43 +1,54 @@
-#include <iostream> 
-#include <vector> 
+#include <iostream>
+#include <vector>
 #include <queue>
 
-int solve(int n){
+int solve(int n)
+{
 
-    int count = 0;
-    int potion;
-    int health = 0;
+    long long count = 0;
+    long long potion;
+    long long health = 0;
 
-    std::priority_queue<int> potions;
+    std::priority_queue<long long> potions;
 
-    for (int i =0; i<n; i++){
-        std::cin>> potion;
-        potions.push(potion);
-    }
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> potion;
+        health += potion;
+        count++;
+        potions.push(-potion);
 
-    int no_of_potions = potions.size();
-    for (int i = 0; i<no_of_potions;i++){
+        while (health < 0)
+        {
+        count--;
         health += potions.top();
-        // std::cout<< "Health : " << health<< "\n";
         potions.pop();
-        if (health > -1){
-            count++;
         }
-        else{
-            return count;
-        }
-        
     }
+
+    // int no_of_potions = potions.size();
+    // for (int i = 0; i < no_of_potions; i++)
+    // {
+    //     health += potions.top();
+    //     // std::cout<< "Health : " << health<< "\n";
+    //     potions.pop();
+    //     if (health > -1)
+    //     {
+    //         count++;
+    //     }
+    //     else
+    //     {
+    //         return count;
+    //     }
+    // }
     return count;
 }
 
-int main() {
+int main()
+{
 
-    int n; 
-    std::cin >> n; 
+    int n;
+    std::cin >> n;
 
     std::cout << solve(n);
-
-
-
 }
