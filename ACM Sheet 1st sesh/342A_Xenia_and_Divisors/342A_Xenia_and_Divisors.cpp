fde84ead9ff6d22ab59b9c3053ecef7dcf596d69
Ezz Eldeen <ezz.eldeen.mohammad.9@gmail.com>
2025-04-30 14:45:15 +0300

    Submit 342A_Xenia_and_Divisors

--- ACM Sheet 1st sesh/342A_Xenia_and_Divisors/342A_Xenia_and_Divisors.cpp
+++ ACM Sheet 1st sesh/342A_Xenia_and_Divisors/342A_Xenia_and_Divisors.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
@@ -6,107 +7,29 @@ int main()
     int n;
     std::cin >> n;
 
-    int c_1 = 0, c_2 = 0, c_3 = 0, c_4 = 0, c_5 = 0, c_6 = 0, c_7 = 0;
-
-    for (int i = 0; i < n; i++)
+    std::vector<int> v(8);
+    for (int i = 0, a; i < n; i++)
     {
-        int a;
-        std::cin >> a;
+        scanf("%d", &a);
+        v[a]++;
+    }
 
-        switch (a)
+    if (v[1] == (n / 3) && v[5] == 0 && v[7] == 0 && v[3] <= v[6] && v[2] == (v[4] + (v[6] - v[3])))
+    {
+        while (v[4]--)
         {
-        case 1:
-            c_1++;
-            break;
-        case 2:
-            c_2++;
-            break;
-
-        case 3:
-            c_3++;
-            break;
-
-        case 4:
-            c_4++;
-            break;
-        case 5:
-            c_5++;
-            break;
-
-        case 6:
-            c_6++;
-            break;
-
-        case 7:
-            c_7++;
-            break;
-
-        default:
-            std::cout << "Defult case \n";
-            break;
+            printf("1 2 4 \n");
+            v[2]--;
+        }
+        while (v[2]--){
+            printf("1 2 6 \n");
+        }
+        while(v[3]--){
+            printf("1 3 6 \n");
         }
     }
 
-    int group_count = c_1;
-    bool two_flag = 1;
-    bool found_output = 0;
-    if (group_count){
-    for (int i = 0; i < group_count; i++)
-    {
-        // std::cout << "Iteration no.: " << i << "\n";
-        // std::cout << "c_1 " << c_1 << "\n";
-        if (c_1){
-        if (c_2 && two_flag)
-        {
-            if (c_4){
-                    std::cout << 1 << " " << 2 << " " << 4 << "\n";
-                    c_1--;
-                    c_2--;
-                    c_4--;
-                    found_output =1;
-
-                
-             }           
-            else if(c_6){
-                std::cout << 1 << " " << 2 << " " << 6 << "\n";
-                c_1--;
-                c_2--;
-                c_6--;
-                found_output =1;
-
-
-             }
-            else{
-                // std::cout<< "2 didn't find a suitable partner ;)";
-                group_count++;
-                two_flag = !two_flag;
-
-            }
-
-        }
-    else if(c_3 && c_6){
-        std::cout << 1 << " " << 3 << " " << 6 << "\n";
-        c_1--;
-        c_3--;
-        c_6--;
-        found_output =1;
-
-
-
-    }
-
-    else if(!found_output)
-    {
-        // std::cout << "Wat, how did you get here? \n";
-        std::cout << -1;
-        return 0;
-    }
-    }
-}}
-else {
-    std::cout << -1;
-return 0;
-}
-
+    else
+        printf("-1\n");
     return 0;
 }
